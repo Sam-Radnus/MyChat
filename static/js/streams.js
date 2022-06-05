d6d526ef16848b1eeb1be06553f3322b53d30798
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-06-05 21:52:43 +0530

    Added Audio and Video Tracks

---
+++ static/js/streams.js
@@ -0,0 +1,26 @@
+const APP_ID = '121ed57cbab846018e6a35749de3c958'
+const CHANNEL = 'main'
+const TOKEN = '006121ed57cbab846018e6a35749de3c958IACjgCN9jmIwZXP0pN7DzwAjtQfv20ic8iGnEArOl2UNIGTNKL8AAAAAEACXVkQugiKeYgEAAQCDIp5i'
+const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
+let UID;
+let localTracks = [] //audio and video tracks
+let remoteUsers = {}
+let joinAndDisplayLocalStream = async () => {
+    UID = await client.join(APP_ID, CHANNEL, TOKEN, null) //helps to join a channel 
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()//take audio and video tracks from user
+    
+    let player =`
+    <div class="video-container" id="user-container-${UID}">
+    <div class="username-wrapper"><span class="user-name">My Name</span></div>
+    <div class="video-player" id="user-${UID}"></div>
+    </div> 
+                `
+    
+    
+   document.getElementById('video-streams').insertAdjacentElement('beforeend',player)   
+   localTracks[1].play(`user-${UID}`)//creates a video where our video will be displayed       
+
+   await client.publish([localTracks[0],localTracks[1]]) //helps others see the video 
+}
+
+joinAndDisplayLocalStream()
